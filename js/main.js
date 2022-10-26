80793f85847e42efff45407771d18e4e84ec873a
poletaev21 <poletaev.21@gmail.com>
2022-10-26 12:06:05 +0300

    вынес data в константы, изменил наименования функций

--- js/main.js
+++ js/main.js
@@ -1,32 +1,56 @@
-const getRandom = (min, max, decimalPlaces) => {
-  if (min < 0 || max < 0 || min === max)
-  {
+const TIMES = ['12:00', '13:00', '14:00'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const TITLE = 'Трешка в центре';
+const OFFER_COUNT = 10;
+const PRICE_MIN = 100;
+const PRICE_MAX = 100;
+const GUESTS_MAX = 10;
+const ROOMS_MAX = 10;
+const DESCRIPTION =
+  'Сдаём нашу любимую квартиру в связи с необходимостью расширения. Удобная кухня - гостиная 14,5 м² остаётся новым хозяевам со всей мебелью и техникой. Также оставим мебель в коридоре и спальне, а также технику в ванной. Квартира удачно расположена на втором этаже , на первом прямо под квартирой колясочная зона.';
+
+let avatarIndex = 0;
+
+const getRandomNumber = (min, max, decimalPlaces) => {
+  if (min < 0 || max < 0 || min === max) {
     return NaN;
   }
 
-  if (min>max)
-  {
-    [max, min] = [min,max];
+  if (min > max) {
+    [max, min] = [min, max];
   }
 
-  if (decimalPlaces){
+  if (decimalPlaces) {
     return +(Math.random() * (max - min) + min).toFixed(decimalPlaces);
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-const checkinTime = ['12:00', '13:00', '14:00'];
-const typeHotel = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const createAvatar = (index) => {
+  if (index < 10) {
+    index = `0${index}`;
+  }
+  return `img/avatars/user${index}.png`;
+};
 
 const pickRandom = (inputArray) => {
   const array = [];
   inputArray.forEach((element) => {
-    if (getRandom(0, 1) > 0) {
+    if (getRandomNumber(0, 1) > 0) {
       array.push(element);
     }
   });
@@ -34,41 +58,38 @@ const pickRandom = (inputArray) => {
 };
 
 const createOffer = () => {
+  avatarIndex++;
   const location = {
-    lat: getRandom(35.65000, 35.70000, 5),
-    lng: getRandom(139.70000, 139.80000, 5)
+    lat: getRandomNumber(35.65, 35.7, 5),
+    lng: getRandomNumber(139.7, 139.8, 5),
   };
   return {
     author: {
-      avatar: `img/avatars/user${getRandom(0, 10)}.png`
+      avatar: createAvatar(avatarIndex),
     },
     offer: {
-      title: 'Трешка в центре',
+      title: TITLE,
       address: `${location.lat}, ${location.lng}`,
-      price: getRandom(0, 10000),
-      type: typeHotel[getRandom(0, 4)],
-      rooms: getRandom(1, 10),
-      guests: getRandom(1, 10),
-      checkin: checkinTime[getRandom(0, 2)],
-      checkout: checkinTime[getRandom(0, 2)],
-      features: pickRandom(features),
-      description: `Сдаём нашу любимую квартиру в связи с необходимостью расширения.
-      Удобная кухня - гостиная 14,5 м² остаётся новым хозяевам со всей мебелью и техникой.
-      Также оставим мебель в коридоре и спальне, а также технику в ванной.
-      Квартира удачно расположена на втором этаже , на первом прямо под квартирой колясочная зона.`,
-      photos: pickRandom(photos)
+      price: getRandomNumber(PRICE_MIN, PRICE_MAX),
+      type: TYPES[getRandomNumber(0, (TYPES.length-1))],
+      rooms: getRandomNumber(1, ROOMS_MAX),
+      guests: getRandomNumber(1, GUESTS_MAX),
+      checkin: TIMES[getRandomNumber(0, (TIMES.length-1))],
+      checkout: TIMES[getRandomNumber(0, (TIMES.length-1))],
+      features: pickRandom(FEATURES),
+      description: DESCRIPTION,
+      photos: pickRandom(PHOTOS),
     },
-    location
+    location,
   };
 };
 
-const offers = (number) => {
+const createOffers = (number) => {
   var array = [];
-  for (var i = 0; i<number;i++)
-  {
+  for (var i = 0; i < number; i++) {
     array.push(createOffer());
-  };
+  }
   return array;
-}
+};
 
-offers(10);
+console.log(createOffers(OFFER_COUNT));
